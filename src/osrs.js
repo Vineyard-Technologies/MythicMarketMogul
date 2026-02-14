47a61d87968707933bddfdde5a9c407f61821687
Andrew Rogers <Laserwolve@gmail.com>
2026-02-13 21:04:41 -0800

    fixed osrs image path issue

--- src/osrs.js
+++ src/osrs.js
@@ -472,7 +472,7 @@ function generateEmailReport(osrsData) {
               <table>
                 <tr>
                   <td style="width: 32px; vertical-align: top;">
-                    <img src="https://secure.runescape.com/m=itemdb_oldschool/a=13/1762340401310_obj_sprite.gif?id=${item.id}" alt="${item.name}">
+                    <img src="https://secure.runescape.com/m=itemdb_oldschool/obj_sprite.gif?id=${item.id}" alt="${item.name}">
                   </td>
                   <td class="grid-item-content" style="vertical-align: top;">
                     <h4><a href="https://secure.runescape.com/m=itemdb_oldschool/viewitem?obj=${item.id}" target="_blank" style="color: #333; text-decoration: none;">${item.name}</a></h4>
